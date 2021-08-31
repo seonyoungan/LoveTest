e9fb0bd309e0707e948ae97533de69589764e640
seonyoungan <asy093@naver.com>
2021-08-31 19:39:19 +0900

    first commit

---
+++ app/src/main/java/com/example/lovetest_/fragment/QuestionFragment.kt
@@ -0,0 +1,43 @@
+package com.example.lovetest_.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.lovetest_.R
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_question.*
+
+
+class QuestionFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController : NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController= Navigation.findNavController(view)
+
+        btn_next2.setOnClickListener(this)
+
+        }
+
+    override fun onClick(v:View?){
+        when(v?.id){
+            R.id.btn_next2 -> {
+                navController.navigate(R.id.action_questionFragment_to_selectFragment)
+            }
+        }
+    }
+}
